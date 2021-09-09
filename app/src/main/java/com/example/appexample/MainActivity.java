bc097897c73d5f2b8a29d0e73e1b6939afad29f3
ctm93 <al0ve93@aol.com>
2021-09-09 08:23:43 -0700

    test

---
+++ app/src/main/java/com/example/appexample/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.appexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //this is a test
+    }
+}
